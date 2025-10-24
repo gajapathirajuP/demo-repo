c901ff0b805ce0477d86d4b42b2ef8fdccc57cc2
gajapathirajuP <gajapathiraju.kshatriya@gmail.com>
2025-10-24 10:38:31 +0530

    Operators

---
+++ Operators/demo.py
@@ -0,0 +1,18 @@
+# Operators  ( operators are special symbols performs operations on variable and their values) manipulate data 
+
+# There are diff type of operators  
+
+# Arthimetic operators (+ - * /) Mathematical calculations
+
+#Interpolation : This technique replaces place holders with actual value dynamically using f-strings (formatted string literals) use F with print and {}
+#as place holders
+num1 = 10
+num2 = 5
+
+print("The sum of Two Numbers:",num1+num2)
+
+print("-------")
+
+print(f"The sum of {num1} and {num2} is {num1+num2}")
+
+print("-------")
