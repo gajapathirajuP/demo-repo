d325fa475eb009bb41f7804e84aaaf289319bc1f
gajapathirajuP <gajapathiraju.kshatriya@gmail.com>
2025-10-24 09:23:22 +0530

    Variables

--- README.md
+++
@@ -1,2 +0,0 @@
-# demo-repo
-Sample repo
---
+++ Variables/demo.py
@@ -0,0 +1,21 @@
+# Variables - named storage location in RAM which holds value or data.(Student_name/age/gpa is a variable) computer-> Memory-> Variable ( Syntax variable_name = value)
+# = is assignment operator  and value is data what we store
+
+# Variables
+# Variables allows you to store data or assign data or retrieve data or manipulate data(If required)
+
+#Assign Data
+
+Student_name = 'Raju' # String (datatype)
+Student_age = 35 # Integer (datatype)
+Student_gpa = 9.0 # Float (datatype)
+Student_passed = True # boolean True/False (datatype)
+student_unknown_data = None # represents no value
+
+# Retrieve data
+
+print (Student_name)
+print (Student_age)
+print (Student_gpa)
+print (Student_passed)
+print (student_unknown_data)
