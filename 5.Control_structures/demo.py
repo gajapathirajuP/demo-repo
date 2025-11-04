0683022ee6c0ec42fd8a2506d36ce11a1305668d
gajapathirajuP <gajapathiraju.kshatriya@gmail.com>
2025-11-04 09:04:58 +0530

    demo.py

--- 5.Control_structures/demo.py
+++ 5.Control_structures/demo.py
@@ -105,7 +105,101 @@ age = int(input("Enter Your age: "))
 if age >= 18:
     print("You can vote")
 else:
-    print("You can't vote")    
+    print("You can't vote")   
+
+
+# Terenary operator / conditional expression: it provides a concise way to write an if-else statement on a single line.
+                      # It evaluates a condition and returns one of two values based on whether the condition is true or false.    
+
+# Syntax : value_if_true if condition else value_if_false     
+
+age = int(input("Enter Your age: "))
+
+Status = "You can vote" if age >= 18 else "You can't vote" 
+
+print(Status)
+
+# elif ladder
+# elif (elase if) : It is used in conditional statements to check for multiple conditions sequentially after an intial "if " statement.
+
+#  -> syntax 
+            #if condition:
+                #statements
+            #elif condition:
+                #statements
+            #elif condition:
+                #statements
+            # .    
+            # .    
+           #else:
+                #statements
+
+marks = int(input("Enter your marks: "))
+if marks >= 90:
+    print("A")
+elif marks >= 75:
+    print("B")
+elif marks >= 60: 
+    print("C") 
+elif marks >= 50: 
+    print("D")  
+elif marks >= 35: 
+    print("E")        
+else:
+    print("F")    
+
+# match-case ( available only from python 3.X)    
+# like elif we have match-case in pyhton for checking multiple conditions
+# In other languages like java / c++ we had (switch case)
+# Offers a more expressive and readable alternative to complex if-elif-else chains
+#syntax
+            #match expression:
+                #case pattern1:
+                    # Code block 1
+                #case pattern2:
+                    # Code block 2
+                # ...
+                #case _:  # Optional wildcard/default case
+                    # Default code block
+
+choice = int(input("Enter Your Choice: "))   
+match choice:
+    case 1:
+        print("Option 1 selected")
+    case 2:
+        print("Option 2 selected")  
+    case 3:
+        print("Option 3 selected")  
+    case 4:
+        print("Option 4 selected")   
+    case _:
+        print("Default Option selected")     
+
+ #Nested Conditionals 
+ #Conditionals inside Conditionals                              
+
+# Voting app with identity verification -> incorrect approach
+age = int(input("Enter Your Age: "))
+has_id = input("Do You Have ID (yes/no): ")
+if age >=18 :
+    print("You can vote")
+elif has_id == "yes":
+    print("You can vote as you have ID")
+else:
+    print("You cannot vote")
+
+# Voting app with identity verification using nested conditionals 
+age = int(input("Enter Your Age: "))
+if age >=18 :
+    has_id = input("Do You Have ID (yes/no): ")
+    if has_id == "yes":
+       print("You can vote")
+    else:
+        print("You need an ID to vote") 
+else:
+    print("You cannot vote")
+
+
 
 
     
