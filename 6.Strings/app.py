e160e6ec408ee9b02be0f404cd77a1f4bf3b47a9
gajapathirajuP <gajapathiraju.kshatriya@gmail.com>
2025-11-05 12:04:59 +0530

    app

---
+++ 6.Strings/app.py
@@ -0,0 +1,30 @@
+# Enhanced Student Grade Tracker
+
+print("---"*40)
+print("         Enhanced Student Grade Tracker")
+print("---"*40)
+
+
+# Validate ID
+
+student_id_valid = False
+
+while not student_id_valid:
+    student_id = input("Enter Id: ")
+
+    # check for validations 
+    if student_id.startswith("-") and student_id[1:].isdigit():
+        print("Please Enter Positive Number Only")
+    elif student_id.isdigit():
+        student_id = int(student_id)
+        if student_id > 0:
+            student_id_valid = True
+        else: print("Zero Not Allowed")
+    else: 
+         print("Only Numerics Allowed")
+
+print("Student ID: ",student_id)   
+print("System Generated ID: " +"EDIFY-LEARNER-"+str(student_id))    
+             
+
+
