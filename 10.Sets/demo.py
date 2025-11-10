bc6afcf1339349c9b508a203e2f5b41513e961ba
gajapathirajuP <gajapathiraju.kshatriya@gmail.com>
2025-11-10 18:57:49 +0530

    sets

---
+++ 10.Sets/demo.py
@@ -0,0 +1,124 @@
+  # Set 
+
+        # Set is a data structure which allows you to store multiple values in a single variable, each value is separated with , 
+
+        # Sets are represented using { } or using set  class
+
+        # Accessing Data In Sets 
+
+            # NOTE: Sets don't have INDEX 
+
+            # Doesn't Support Positive & Negative Index 
+
+            # Doesn't Support Indexing & Slicing
+
+        # Looping we can apply 
+
+        # Operators we can apply 
+
+        # Conditionals we can apply 
+
+        # Duplicates Not allowed in Sets 
+
+        # Order is not preserved in Sets
+
+        # Sets are UUU 
+
+            # U - Unique Data Ony  (No Duplicates)
+
+            # U - Unindex 
+
+            # U - Unordered  
+
+    # Sets are specifically used with math sets operations 
+
+    # Sets support different methods for performing operations on data 
+
+    # Frozen Set 
+
+        #  Frozen Sets are UUU 
+
+            # U - Unique Data Ony  (No Duplicates)
+
+            # U - Unindex 
+
+            # U - Unordered  
+
+        # NOTE : Frozen Set are Immutable Unlike Sets 
+
+        # Frozen Sets cannot be represented using symbols, only with class we can create 
+
+        # NOTE : Frozen Set only supports math related set operations 
+
+# Sets 
+
+# Empty Set 
+
+# empty set ==> by default if we use empty {} then it's a dict 
+empty_set = {}
+print(empty_set)
+print(type(empty_set))
+
+# empty set 
+empty_set = set()
+print(empty_set)
+print(type(empty_set))
+
+# set with numeric data 
+data = {10,20,30,40,50} 
+print(type(data))
+print(data)
+
+# set with text data 
+data = {"python","django","sql"}
+print(data)
+
+# set with mixed data 
+data = {10,20,30,"python","django","sql",3.5,True}
+print(data)
+
+# Accessing Data 
+data = {10,20,30,40,50}
+print(data)
+
+# print(data[0])
+# print(data[10])
+
+# access individual elements 
+data = {10,20,30,40,50}
+for num in data:
+    print(num)
+    
+# apply operators 
+data = {10,20,30,40,50}
+for num in data:
+    print(num * 10)
+    
+# apply conditionals 
+data = {10,25,30,45,55}
+print(data)
+for num in data:
+    if num % 2 == 0:
+        print(num)
+
+# we can work with any type of data and apply appropriate methods
+data = {"python","django","sql"}
+print(data)
+for course in data:
+    print(course.upper())
+    
+# Behavior of duplicates 
+data = {10,20,10,30,20,40,10,50}
+print(data)
+
+# sets methods 
+print(dir(data))
+
+# Frozen Set
+data = {10,20,10,30,20,40,10,50}
+print(type(data))
+
+data_fz_set =frozenset({10,20,10,30,20,40,10,50})
+print(type(data_fz_set))
+
+print(dir(data_fz_set))
